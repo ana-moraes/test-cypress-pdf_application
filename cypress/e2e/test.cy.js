2063b273e122a4fed53dcdff1d9b9ea264e88b19
Ana Carolina M Oliveira <ana.moraesoliveira@gmail.com>
2025-11-08 20:43:18 -0300

    feature: Validate email is received

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -21,3 +21,21 @@ it('Upload image files', () => {
         })
     })
 })
+
+it('Verify email sent', () => {
+    cy.visit(Cypress.env('yopmail'))
+    cy.get('body').then(($body) => {
+      const $btn = $body.find('[aria-label="Consent"]')
+      if ($btn.length && $btn.is(':visible')) {
+        cy.wrap($btn).click({ force: true })
+      }
+    });
+    cy.get('[class="ycptinput"]').type(Cypress.env('email'))
+    cy.get('[id="refreshbut"]').click()
+    cy.get('iframe#ifmail', { timeout: 15000 }).should('exist').then(($iframe) => {
+      const body = $iframe.contents().find('body')
+      cy.wrap(body)
+        .find('a[title*="'+Cypress.env('file_name')+'"]', { timeout: 10000 })
+        .should('be.visible')
+    })
+})
